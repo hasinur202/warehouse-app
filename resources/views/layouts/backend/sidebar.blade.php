f1c858c494bd7f0437ab0c9aa69e818334c6467d
Hasinur Rahman <hasinur202@gmail.com>
2021-03-24 15:09:29 +0600

    logo show in admin dashboard

--- resources/views/layouts/backend/sidebar.blade.php
+++ resources/views/layouts/backend/sidebar.blade.php
@@ -2,9 +2,13 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="{{ route('dashboard') }}" class="brand-link">
+    {{--  <a href="{{ route('dashboard') }}" class="brand-link">
       <img src="/backend/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">Admin Panel</span>
+    </a>  --}}
+
+    <a href="{{ route('dashboard') }}" class="brand-link">
+        <img src="/logo/logo.png" alt="AdminLTE Logo" style="width:100%;height:100%;background:#fff">
     </a>
 
     <!-- Sidebar -->

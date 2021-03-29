a3b1e957de479d552a35cc0014e77e28ddaae191
Hasinur Rahman <hasinur202@gmail.com>
2021-03-29 13:47:54 +0600

    slider mvc create

---
+++ database/migrations/2021_03_29_070041_create_slides_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSlidesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('slides', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('warehouse_id')->nullable()->constrained('warehouses')->onDelete('cascade');
+            $table->string('image');
+            $table->string('slug');
+            $table->text('description');
+            $table->string('url')->default('#');
+            $table->boolean('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('slides');
+    }
+}
